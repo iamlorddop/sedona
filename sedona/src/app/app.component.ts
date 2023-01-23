5a27e106ebf9ec5d4cb0800f68a212aa817383ad
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-01-23 19:23:06 +0300

    add popup

--- sedona/src/app/app.component.ts
+++ sedona/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'sedona';
+  goScroll(target: HTMLElement) {
+    target.scrollIntoView({behavior: "smooth"});
+  }
 }
